b286d377a6c1aac2acb2dd35af48bf0dd07585e1
Kris <kris@lemone.com>
2025-02-10 21:20:36 +0100

    style: clean up index.html.erb by removing unused turbo_frame_tag
    Removed the turbo_frame_tag for 'new_tasks' as it was not being used,
    simplifying the view.
    
    Format code in the tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,15 @@
 class TasksController < ApplicationController
-
   def index
     @tasks = Task.open.order(created_at: :desc)
     @completed_tasks = Task.completed.order(created_at: :desc)
   end
-end
+
   private
-  def set_task
-    @task = Task.find(params[:id])
-  end
-  def task_params
-    params.require(:task).permit(:title, :description, :status)
-  end
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:title, :description, :status)
+    end
 end
