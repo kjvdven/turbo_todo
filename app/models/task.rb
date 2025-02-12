d15f96795912ce15bfd18c9f0264915a6bdbed27
Kris <306806+kjvdven@users.noreply.github.com>
2025-02-12 21:23:20 +0100

    feat: improve broadcasting for task update status
    Change broadcasting so that on a task status changes the task is moved
    between in_progress and completed lists.
    
    - Create methods for handling create, update, and destroy events
    - Change broadcasting to method-based broadcasting
    - Handle status updates broadcasting

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,9 @@
 class Task < ApplicationRecord
-  broadcasts_to ->(task) { :tasks }, inserts_by: :prepend
+  after_create_commit :broadcast_create
+  after_update_commit :broadcast_update
+  after_destroy_commit :broadcast_destroy
+
+  # broadcasts_to ->(task) { :tasks }, inserts_by: :prepend
 
   enum :status, { in_progress: "in_progress", completed: "completed",  archived: "archived" }
 
@@ -8,4 +12,22 @@ class Task < ApplicationRecord
   validates :status, inclusion: { in: statuses.keys }
 
   scope :ordered, -> { order(id: :desc) }
+
+  private
+  def broadcast_create
+    broadcast_prepend_to "tasks", target: "in_progress_tasks"
+  end
+
+  def broadcast_update
+    if saved_change_to_status?
+      broadcast_remove_to "tasks"
+      broadcast_prepend_to "tasks", target: completed? ? "completed_tasks" : "in_progress_tasks"
+    else
+      broadcast_replace_to "tasks"
+    end
+  end
+
+  def broadcast_destroy
+    broadcast_remove_to "tasks"
+  end
 end
