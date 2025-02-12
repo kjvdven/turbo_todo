31acaa4ab98ffb4c733fb6a4d7adbe4e55ef75fa
Kris <kris@lemone.com>
2025-02-12 10:34:58 +0100

    feat: update cable configuration for development environment
    - Changed adapter from async to solid_cable
    - Added connection details and polling intervals
    - Set message retention period

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
+  config.solid_queue.logger = ActiveSupport::Logger.new(STDOUT)
 end
